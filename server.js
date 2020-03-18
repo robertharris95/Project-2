d48b02f175c44fb7c681641d1d8cb53c1c50e49b
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-18 11:11:23 -0500

    bcryptjs, express-session and passport

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-var app = express();
+const app = express();
+const db = require("./models");
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,11 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(session({ secret: "we sail", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+// require("ROUTES")
 
-app.listen(PORT, () => {
-    console.log(`Server listening on: http://localhost:${PORT}`);
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, () => {
+        console.log(`Server listening on: http://localhost:${PORT}`);
+    });
 });
