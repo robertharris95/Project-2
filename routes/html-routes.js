c19940d28b67891013f684602a8fe82ef710ea2b
Robert Harris <rlh9@rice.edu>
2020-03-19 11:41:48 -0500

    Login function not working, html edited for navbar functionality

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
   app.get("/signup", function(req, res) {
