c2a03b2e73a59eca9c2ff5b93568ba997d61f469
Robert Harris <rlh9@rice.edu>
2020-03-19 11:45:51 -0500

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,27 +8,33 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+ 
     res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/seller");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/seller");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/seller", isAuthenticated, function(req, res) {
     console.log(req);
     Product.findAll({})   // @@ relate every product to its company foreign key pass company data in same order.
     .then( (data) => {    
-      res.render("index", { product: data });
+      res.sendFile(path.join(__dirname, "../public/seller.html"));
     });
   });
 };
